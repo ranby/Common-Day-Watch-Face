3d1603c527845190e26405893cf1623510914e19
ranby <erik.ranby@gmail.com>
2015-09-13 13:35:21 +0200

    Analog watch not shows working time

--- wear/src/main/java/com/ejjan/commondaywatch/WatchFaceService.java
+++ wear/src/main/java/com/ejjan/commondaywatch/WatchFaceService.java
@@ -1,17 +1,24 @@
 package com.ejjan.commondaywatch;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.wearable.watchface.CanvasWatchFaceService;
 import android.support.wearable.watchface.WatchFaceStyle;
 import android.view.SurfaceHolder;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by Erik on 12/09/15.
@@ -27,6 +34,39 @@ public class WatchFaceService extends CanvasWatchFaceService {
     /* implement service callback methods */
     private class Engine extends CanvasWatchFaceService.Engine {
 
+        private static final int MSG_UPDATE_TIME = 42;
+        private static final long DEFAULT_UPDATE_RATE_MS = 1000*60;
+
+        private boolean hasTimeZoneReceiverBeenRegistered = false;
+
+        Calendar calendar;
+
+        final Handler updateTimeHandler = new Handler() {
+            @Override
+            public void handleMessage(Message message) {
+                switch (message.what) {
+                    case MSG_UPDATE_TIME:
+                        invalidate();
+                        System.out.println("Invalidated due to timer message");
+                        if (shouldTimerBeRunning()) {
+                            long timeMs = System.currentTimeMillis();
+                            long delayMs = DEFAULT_UPDATE_RATE_MS
+                                    - (timeMs % DEFAULT_UPDATE_RATE_MS);
+                            updateTimeHandler.sendEmptyMessageDelayed(MSG_UPDATE_TIME, delayMs);
+                        }
+                        break;
+                }
+            }
+        };
+
+        final BroadcastReceiver timeZoneReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                calendar.setTimeZone(TimeZone.getDefault());
+                invalidate();
+            }
+        };
+
         @Override
         public void onCreate(SurfaceHolder holder) {
             super.onCreate(holder);
@@ -37,6 +77,8 @@ public class WatchFaceService extends CanvasWatchFaceService {
                             .setShowSystemUiTime( false )
                             .build()
             );
+
+            calendar = Calendar.getInstance();
         }
 
         @Override
@@ -49,6 +91,8 @@ public class WatchFaceService extends CanvasWatchFaceService {
         public void onTimeTick() {
             super.onTimeTick();
             /* the time changed */
+            System.out.println("Time tick...");
+            invalidate();
         }
 
         @Override
@@ -60,6 +104,7 @@ public class WatchFaceService extends CanvasWatchFaceService {
         @Override
         public void onDraw(Canvas canvas, Rect bounds) {
             /* draw your watch face */
+
             System.out.println("Drawing...");
 
             int height = canvas.getHeight();
@@ -67,10 +112,12 @@ public class WatchFaceService extends CanvasWatchFaceService {
             int centerx = width/2;
             int centery = height/2;
 
-            Date date = new Date();
-            int mins = date.getMinutes();
-            int hours = date.getHours();
+//            currentTime.setToNow();
+            calendar.setTime(new Date());
+            int mins = calendar.get(Calendar.MINUTE);
+            int hours = calendar.get(Calendar.HOUR_OF_DAY);
 
+            canvas.drawColor(Color.TRANSPARENT , PorterDuff.Mode.CLEAR);
             drawTickets(canvas, centerx, centery);
             drawHourHand(canvas, centerx, centery, hours, mins);
             drawMinuteHand(canvas, centerx, centery, mins);
@@ -80,6 +127,46 @@ public class WatchFaceService extends CanvasWatchFaceService {
         public void onVisibilityChanged(boolean visible) {
             super.onVisibilityChanged(visible);
             /* the watch face became visible or invisible */
+            if (visible) {
+                registerReceiver();
+
+                // Update time zone in case it changed while we weren't visible.
+                calendar.setTimeZone(TimeZone.getDefault());
+            } else {
+                unregisterReceiver();
+            }
+
+            // Whether the timer should be running depends on whether we're visible and
+            // whether we're in ambient mode, so we may need to start or stop the timer
+            updateTimer();
+        }
+
+        private void updateTimer() {
+            updateTimeHandler.removeMessages(MSG_UPDATE_TIME);
+            if (shouldTimerBeRunning()) {
+                updateTimeHandler.sendEmptyMessage(MSG_UPDATE_TIME);
+            }
+        }
+
+        private void registerReceiver() {
+            if (hasTimeZoneReceiverBeenRegistered) {
+                return;
+            }
+            hasTimeZoneReceiverBeenRegistered = true;
+            IntentFilter filter = new IntentFilter(Intent.ACTION_TIMEZONE_CHANGED);
+            WatchFaceService.this.registerReceiver(timeZoneReceiver, filter);
+        }
+
+        private void unregisterReceiver() {
+            if (!hasTimeZoneReceiverBeenRegistered) {
+                return;
+            }
+            hasTimeZoneReceiverBeenRegistered = false;
+            WatchFaceService.this.unregisterReceiver(timeZoneReceiver);
+        }
+
+        private boolean shouldTimerBeRunning() {
+            return isVisible() && !isInAmbientMode();
         }
 
         private void drawHourHand(Canvas canvas, int centerx, int centery, int hours, int minutes) {
